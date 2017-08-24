0a89fe4f4b0b1f1841dedffb7d3c1bafc14c6313
kevinmeng1022 <31265834+kevinmeng1022@users.noreply.github.com>
2017-08-23 21:27:37 -0700

    my first commit
    tested it and works correctly

---
+++ question2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+ class hw {
+
+    private static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int input1 = sc.nextInt();
+        int input2 = sc.nextInt();
+        System.out.println("The sum of "+input1+" and " + input2 + " is " 2 5+ (input1 + input2));
+    }
+}
